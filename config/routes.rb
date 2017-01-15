4db9c6e08c5ad9a11efc0c16a0528c7573e1849e
kaiwen2times <kxz6582@rit.edu>
2017-01-14 20:36:05 -0500

    Finalizing version 1.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	root 	"static_pages#home"
+	root 	"static_pages#resume"
   get 	"/resume", to: "static_pages#resume"
 	get 	"/contact", to: "static_pages#contact"
 
